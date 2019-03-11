46f27511e0e472ce65c407ca6b2bde1c67fce46a
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-03-11 19:22:35 -0400

    Side-track : Bottles of Beer on the Wall in Ruby

---
+++ chris pines refresher/00 - misc.rb
@@ -0,0 +1,17 @@
+numBottles = 99
+
+
+while numBottles > 2
+  puts "#{numBottles} bottles of beer on the wall. #{numBottles} bottles of beer."
+  puts "Take one down, pass it around. \n#{numBottles - 1} bottles of beer on the wall."
+  numBottles -= 1
+end
+
+
+puts "#{numBottles} bottle of beer on the wall. #{numBottles} bottle of beer."
+puts "Take one down, pass it around. \n#{numBottles} bottle of beer on the wall."
+numBottles -= 1
+
+
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
