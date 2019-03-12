3edcd71dd64c1f1d09cfd1b33f345908c0e24705
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-03-11 22:14:17 -0400

    Modules work on movieApp

--- movieApp/playlist.rb
+++ movieApp/playlist.rb
@@ -1,4 +1,5 @@
 require_relative 'movie'
+require_relative 'waldorf_and_statler'
 
 class Playlist
 
@@ -13,9 +14,7 @@ class Playlist
     @movies << a_movie
   end
 
-  def roll_die
-    rand(1..6)
-  end
+
 
   def play
     puts "#{@name}'s playlist:"
@@ -23,18 +22,23 @@ class Playlist
     puts @movies
 
     @movies.each do |movie|
-      case roll_die
-      when 1..2
-        movie.thumbs_down
-        puts "#{movie.title} got a thumbs down."
-      when 3..4
-        puts "#{movie.title} was skipped."
-      else
-        movie.thumbs_up
-        puts "#{movie.title} got a thumbs up!"
-      end
+      WaldorfAndStatler.review(movie)
       puts movie
     end
   end
 end
 
+#sample code
+if __FILE__  == $0
+
+  movie1 = Movie.new("Die Hard", 10)
+  movie2 = Movie.new("Beetlejuice", 10)
+  movie3 = Movie.new("Animaniacs", 10)
+
+  playlist = Playlist.new("bobsPlaylist")
+
+
+  playlist.add_movie(movie1)
+  playlist.add_movie(movie2)
+  playlist.play
+end
