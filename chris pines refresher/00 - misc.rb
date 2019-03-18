5544dd5dcca6a36f903deb21a1c1792c2261d3c4
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-03-18 00:32:50 -0400

    Hashes practice and Input and Output.

--- chris pines refresher/00 - misc.rb
+++ chris pines refresher/00 - misc.rb
@@ -1,3 +1,6 @@
+require '/home/osboxes/RubymineProjects/Pragmatic-Ruby-Game/movieApp/movie.rb'
+
+=begin
 numBottles = 99
 
 
@@ -113,3 +116,205 @@ puts "\n\n\n\n"
 Snack = Struct.new(:name, :carbs)
 popcorn = Snack.new("popcorn", 20)
 candy = Snack.new("candy", 15)
+puts "\n\n\n\n"
+
+#Hashes
+#old way of writing before Ruby 1.9
+#snack_carbs = {
+#    :candy => 15,
+#    :pretzel => 10
+#}
+
+#New way of writing hashes (greater than Ruby 1.9)
+snack_carbs = {
+    candy: 30,
+    pretzel: 100
+
+}
+
+puts snack_carbs[:candy]
+
+snack_carbs[:candy] = 15
+puts snack_carbs[:candy]
+
+print snack_carbs.keys
+print snack_carbs.values
+puts""
+
+snack_carbs.each do |key, value|
+  puts "#{key} has #{value} carbs"
+end
+
+puts"\n\n"
+
+snack_carbs = {}
+snack_carbs[:candy] = 15
+snack_carbs[:candy] += 15
+puts snack_carbs[:candy]
+
+#snack_carbs[:surge] += 10
+
+snack_carbs = Hash.new(0)
+snack_carbs[:bread] += 15
+p snack_carbs
+
+
+#Hashes
+
+moes_treasures = {
+    :hammer => 100,
+    :crowbar => 500
+}
+
+puts moes_treasures
+
+moes_treasures.each { |key, value| puts "Moe has a #{key} that's worth #{value}" }
+
+hash_arr = moes_treasures.keys
+print hash_arr
+
+hash_arr = moes_treasures.values
+print hash_arr
+
+puts "\n\n"
+
+#So if we wanted to sum up the values of all the keys, we'd run this.
+#In the parenthesis, is the initial value....(zero)
+#Then you have the sum...for the entire block, and each part that's going to get added up.
+sum1 = moes_treasures.values.reduce(0){ |sum, n| sum + n }
+
+# can also write it with a symbol
+sum2= moes_treasures.values.reduce(0, :-)
+puts sum2
+
+#You can also use INJECT...which is exactly same as reduce.
+sum3 = moes_treasures.values.reduce{|sum, n| sum + n}
+puts sum3
+puts "\n\n\n\n\n"
+=end
+
+
+#Scrabble Game
+#
+
+
+letters = {
+    "c" => 3,
+    "e" => 1,
+    "l" => 1,
+    "n" => 1,
+    "t" => 1,
+    "x" => 8,
+    "y" => 4
+}
+
+score = Hash.new(0)
+
+word = "cell".each_char
+
+word.each do |char|
+
+  letters.each { |key, value|
+    if char == key
+      score[key] += value
+    end
+  }
+
+end
+
+score.each do |key, value|
+  puts "#{key} : #{value}"
+end
+
+#Custom Iterators
+
+def once
+
+  puts "Before Yield"
+  yield
+  puts "After Yield"
+
+end
+
+once {puts "Running Block"}
+
+def three_times
+
+  yield(1)
+  yield(2)
+
+end
+
+three_times {|number| puts "This is number #{number}"}
+
+
+def compute
+  puts yield
+end
+
+
+numbers = (1..10).to_a
+
+####Let's make the array give even numbers only:
+
+numbers = (1..10).to_a
+numbers.select {|num|num.even? }
+
+#But now, let's implement this "SELECT" method ourselves!
+
+def my_select(array)
+  results = []
+  array.each do |element|
+    results << element if yield(element)
+  end
+  results
+end
+
+puts my_select(numbers) {|n|n.even?}
+
+
+
+def conversation
+  puts "Hello"
+  yield
+  puts "Goodbye"
+end
+
+conversation {puts "Good to meet you!"}
+
+def five_times
+  yield 1
+  yield 2
+end
+
+five_times do |n|
+  puts "#{n} situps"
+  puts "#{n} pushups"
+  puts "#{n} chinups"
+end
+
+
+#INPUT OUTPUT
+# Opening Files
+
+File.open("/home/osboxes/RubymineProjects/Pragmatic-Ruby-Game/chris pines refresher/movies.csv") do |file|
+ file.each_line do |line|
+    puts line
+ end
+
+end
+
+#use readlines method instead...it runs a block and puts each line into an array that you can then print.
+File.readlines("/home/osboxes/RubymineProjects/Pragmatic-Ruby-Game/chris pines refresher/movies.csv").each do |line|
+    print line
+end
+
+puts "\n\n\n"
+#use readlines method instead...it runs a block and puts each line into an array that you can then print.
+# Make sure to require the movie object at the top of this file..
+File.readlines("/home/osboxes/RubymineProjects/Pragmatic-Ruby-Game/chris pines refresher/movies.csv").each do |line|
+  title, rank = line.split(",")
+  movie = Movie.new(title, rank.to_i)
+  puts movie
+end
+
